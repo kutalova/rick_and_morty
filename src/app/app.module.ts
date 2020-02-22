e52ffb5dec545da8bba5b674b0344b32e61da4e0
Anastasiia Kutalova <a.kutalova@uplata.com>
2020-02-22 23:52:13 +0200

    added_bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+@NgModule( {
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FontAwesomeModule,
+        NgbModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
+} )
+export class AppModule {
+}
